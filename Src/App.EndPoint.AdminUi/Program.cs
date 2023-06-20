0eb409536178590af1a957ddea57db047c6f18f2
fatemefarokhzad98 <fatemefarokhzad98@gmail.com>
2023-06-20 22:41:26 +0430

    AddRazorRuntimeCompilation

--- Src/App.EndPoint.AdminUi/Program.cs
+++ Src/App.EndPoint.AdminUi/Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+
+
+
+
+
 
 var app = builder.Build();
 
