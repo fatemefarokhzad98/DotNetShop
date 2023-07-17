b909ea6f2d6639f84bdc2c667b54ba0de6d7c57f
fatemefarokhzad98 <fatemefarokhzad98@gmail.com>
2023-07-17 14:02:24 +0330

    AddRazorRuntimeCompilation

---
+++ Src/App.EndPoint/App.EndPoint.ShopUi/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoint.ShopUi.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
